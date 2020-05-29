3043ef8a34f770472b26df591c053fc37b939845
Brondo765 <brondo765@gmail.com>
2020-05-28 21:33:02 -0500

    Added tests for ListWriter, uses a mock to verify the list has been written when writeList method is called.

--- src/test/java/edu/depaul/email/ListWriterTest.java
+++ src/test/java/edu/depaul/email/ListWriterTest.java
@@ -1,10 +1,57 @@
 package edu.depaul.email;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 // Make list writer test and use mock!
 // Pass in object and verify that it contains expected results after calling writeList
 
 class ListWriterTest {
+    private final File file = new File("src\\test\\resources\\writerOutput.txt");
+    private Collection<String> list;
 
+    @BeforeEach
+    void setup() {
+        list = Arrays.asList("bwegner@mail.depaul.edu",
+                "AnotherEmail@gmail.com",
+                "HereBeAnother@yahoo.com",
+                "email@gmail.com",
+                "referenceMail@mail.org");
+    }
+
+    @Test
+    @DisplayName("Basic test for writer to see if file is created when written to")
+    void testBasicUsage() throws IOException {
+        BufferedReader reader = Files.newBufferedReader(Paths.get(String.valueOf(file)));
+        FileOutputStream out = new FileOutputStream(file);
+        ListWriter writer = new ListWriter(out);
+        writer.writeList(list);
+
+        String line;
+        int count = 0;
+        while ((line = reader.readLine()) != null) {
+            if (list.contains(line)) {
+                count++;
+            }
+        }
+        assertEquals(5, count);
+    }
+
+    @Test
+    @DisplayName("Test for mocked ListWriter verifies when writeList is called")
+    void testMockWriter() throws IOException {
+        ListWriter mockWriter = mock(ListWriter.class);
+        mockWriter.writeList(list);
+        verify(mockWriter).writeList(list);
+    }
 }
