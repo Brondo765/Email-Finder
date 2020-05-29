6d321c96159d825048b6f4397f32e4de39f6267f
Brondo765 <brondo765@gmail.com>
2020-05-28 21:36:13 -0500

    Added parameterized tests to PageFetcher for functional testing requirement.

--- src/test/java/edu/depaul/email/PageFetcherTest.java
+++ src/test/java/edu/depaul/email/PageFetcherTest.java
@@ -5,8 +5,14 @@ import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,6 +46,21 @@ class PageFetcherTest {
         assertEquals("Test Page", doc.title());
     }
 
+    @ParameterizedTest
+    @MethodSource("provideFileInput")
+    @DisplayName("Test for get method with multiple local files")
+    void testGetMultipleLocal(String path) throws IOException {
+        File input = new File(path);
+        Document doc = Jsoup.parse(input, String.valueOf(StandardCharsets.UTF_8));
+        assertEquals(String.valueOf(doc).intern(), String.valueOf(fetcher.get(path)).intern());
+    }
+
+    private static Stream<Arguments> provideFileInput() {
+        return Stream.of(
+                Arguments.of("src\\test\\resources\\multiTestPage.html"),
+                Arguments.of("src\\test\\resources\\oneTestPage.html"));
+    }
+
     @Test
     @DisplayName("Test get invalid URL")
     void testGetInvalid() {
@@ -66,6 +87,23 @@ class PageFetcherTest {
         Document doc = Jsoup.connect("http://www.littlewebhut.com/articles/simple_web_page/").get();
         assertSame(doc.outerHtml().intern(), fetcher.getString(
                 "http://www.littlewebhut.com/articles/simple_web_page/").intern());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideWebsiteInput")
+    @DisplayName("Test for multiple connections to websites")
+    void testParameterizedGetStringWeb(String website) throws IOException {
+        Document doc = Jsoup.connect(website).get();
+        assertEquals(doc.outerHtml().intern(), fetcher.getString(website).intern());
+    }
+
+    private static Stream<Arguments> provideWebsiteInput() {
+        return Stream.of(
+                Arguments.of("http://www.cnn.com"),
+                Arguments.of("http://www.cnbc.com"),
+                Arguments.of("http://www.cdm.depaul.edu"),
+                Arguments.of("http://www.basicwebsiteexample.com/"),
+                Arguments.of("https://www.littlepotatoes.com/recipes/"));
     }
 
     @Test
