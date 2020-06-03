2b47fc8012b829920f2ae0944a4c0f639bd36593
Brondo765 <brondo765@gmail.com>
2020-06-03 00:11:37 -0500

    Updated local html files and added more scenarios to parser.feature

--- src/test/java/edu/depaul/email/step_definitions/LinkFinderSteps.java
+++ src/test/java/edu/depaul/email/step_definitions/LinkFinderSteps.java
@@ -1,21 +1,27 @@
 package edu.depaul.email.step_definitions;
 
 import edu.depaul.email.PageParser;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkFinderSteps {
     private Document doc;
 
-    @Given("the path to the html file is {string}")
-    public void the_path_to_the_html_file_is(String path) {
-        doc = Jsoup.parse(path);
+    @Given("the path to the html file for one link is {string}")
+    public void the_path_to_the_html_file_for_one_link_is(String path) throws IOException {
+        File file = new File(path);
+        doc = Jsoup.parse(file, String.valueOf(StandardCharsets.UTF_8));
     }
 
     @Then("the collected link is {string}")
@@ -26,5 +32,50 @@ public class LinkFinderSteps {
             link = str;
         }
         assertEquals("http://www.cnn.com", link);
+    }
+
+    @Given("the path to the html file for multiple links is {string}")
+    public void the_path_to_the_html_file_for_multiple_links_is(String path) throws IOException {
+        File file = new File(path);
+        doc = Jsoup.parse(file, String.valueOf(StandardCharsets.UTF_8));
+    }
+
+    @Then("the collected links are")
+    public void the_collected_links_are(DataTable dataTable) {
+        PageParser parser = new PageParser();
+        List<String> links = dataTable.asList();
+        Set<String> setParsed = parser.findLinks(doc);
+        assertTrue(setParsed.containsAll(links));
+    }
+
+    @Given("the path to the html file for one email is {string}")
+    public void the_path_to_the_html_file_for_one_email_is(String path) throws IOException {
+        File file = new File(path);
+        doc = Jsoup.parse(file, String.valueOf(StandardCharsets.UTF_8));
+    }
+
+    @Then("the collected email is {string}")
+    public void the_collected_email_is(String email) {
+        PageParser parser = new PageParser();
+        Set<String> setParsed = parser.findEmails(doc);
+        for (String str : setParsed) {
+            email = str;
+        }
+        assertEquals("ThisIsAnEmail@yahoo.com", email);
+    }
+
+    @Given("the path to the html file for multiple emails is {string}")
+    public void the_path_to_the_html_file_for_multiple_emails_is(String path) throws IOException {
+        File file = new File(path);
+        doc = Jsoup.parse(file, String.valueOf(StandardCharsets.UTF_8));
+    }
+
+
+    @Then("the collected emails are")
+    public void the_collected_emails_are(io.cucumber.datatable.DataTable dataTable) {
+        PageParser parser = new PageParser();
+        List<String> emails = dataTable.asList();
+        Set<String> setParsed = parser.findEmails(doc);
+        assertTrue(setParsed.containsAll(emails));
     }
 }
--- src/test/resources/parser.feature
+++ src/test/resources/parser.feature
@@ -1,4 +1,24 @@
-Feature: Parser finds the correct links
-  Scenario: Finding a set of links on an html file
-    Given the path to the html file is "src\\test\\resources\\oneTestPage.html"
+Feature: Parser finds the correct links and emails
+  Scenario: Finding one link on an html file
+    Given the path to the html file for one link is "src\test\resources\oneTestPage.html"
     Then the collected link is "http://www.cnn.com"
+
+  Scenario: Finding multiple links on an html file
+    Given the path to the html file for multiple links is "src\test\resources\multiTestPage.html"
+    Then the collected links are
+    | https://this-links-out-somewhere.com  |
+    | ../relativeLink.html                  |
+    | mailto:info@woodridge68.org           |
+    | https://kli.org                       |
+    | http://cnn.com                        |
+
+  Scenario: Finding one email on an html file
+    Given the path to the html file for one email is "src\test\resources\oneTestPage.html"
+    Then the collected email is "ThisIsAnEmail@yahoo.com"
+
+  Scenario: Finding multiple emails on an html file
+   Given the path to the html file for multiple emails is "src\test\resources\multiTestPage.html"
+   Then the collected emails are
+   | SomeEmail@yahoo.com    |
+   | bwegner@depaul.edu     |
+   | AnotherEmail@gmail.com |
